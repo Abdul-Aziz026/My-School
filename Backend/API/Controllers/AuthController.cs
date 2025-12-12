2674ae53812b269190aa2b1ac33a58ceb0bd35f4
DESKTOP-RNS2QJ2\azizu <azizulcsebsmrstu@gmail.com>
2025-12-12 18:30:11 +0600

    add auth controller

---
+++ Backend/API/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class AuthController : Controller
+{
+    [HttpPost("register")]
+    public IActionResult Register()
+    {
+        return Ok("register");
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login()
+    {
+        return Ok("login");
+    }
+    [HttpPost("refresh")]
+    public IActionResult Refresh()
+    {
+        return Ok("refresh");
+    }
+    [HttpPost("logout")]
+    public IActionResult Logout()
+    {
+        return Ok("logout");
+
+    }
+    [HttpGet("me")]
+    public IActionResult Profile()
+    {
+        return Ok("Profile");
+    }
+}

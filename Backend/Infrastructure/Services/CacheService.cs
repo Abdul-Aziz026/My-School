04256c0da64c6545ed7ee25abed0094e7e9421f7
DESKTOP-RNS2QJ2\azizu <azizulcsebsmrstu@gmail.com>
2026-01-02 22:46:14 +0600

    add redis cache service

---
+++ Backend/Infrastructure/Services/CacheService.cs
@@ -0,0 +1,27 @@
+﻿
+using Application.Common.Interfaces.Services;
+
+namespace Infrastructure.Services;
+
+public class CacheService : ICacheService
+{
+    public async Task<bool> ExistsAsync(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<T?> GetAsync<T>(string key) where T : class
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task RemoveAsync(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task SetAsync<T>(string key, T value, TimeSpan? expiration = null) where T : class
+    {
+        throw new NotImplementedException();
+    }
+}

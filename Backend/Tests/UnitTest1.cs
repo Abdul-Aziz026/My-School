b2419e121ae2a5b51b4e471cdb6fcd505fe75b71
DESKTOP-RNS2QJ2\azizu <azizulcsebsmrstu@gmail.com>
2026-01-03 22:45:27 +0600

    add xunit test demo

--- Backend/Tests/UnitTest1.cs
+++ Backend/Tests/UnitTest1.cs
@@ -5,6 +5,22 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        // arrange
 
+        // act
+
+        Assert.Equal(1, 1);
+    }
+
+    [Theory]
+    [InlineData(1, 2, 3)]
+    [InlineData(5, 5, 10)]
+    public void Test2(int x, int y, int expected)
+    {
+        // act
+        var result = x + y;
+
+        // assert
+        Assert.Equal(expected, result);
     }
 }

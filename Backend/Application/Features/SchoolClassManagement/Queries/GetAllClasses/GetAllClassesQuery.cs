b9d718ab52c85357dd68362421ffa4c78fd1f86f
Azizur Rahman <azizur.rahman@orbitax.com>
2026-01-26 19:54:48 +0600

    test commit

---
+++ Backend/Application/Features/SchoolClassManagement/Queries/GetAllClasses/GetAllClassesQuery.cs
@@ -0,0 +1,6 @@
+﻿using Application.Features.SchoolClassManagement.DTOs;
+using MediatR;
+
+namespace Application.Features.SchoolClassManagement.Queries.GetAllClasses;
+
+public record GetAllClassesQuery : IRequest<List<ClassResponseDto>>;
---
+++ Backend/Application/Features/SchoolClassManagement/Queries/GetAllClasses/GetAllClassesQueryHandler.cs
@@ -0,0 +1,20 @@
+﻿using Application.Common.Interfaces.Repositories;
+using Application.Features.SchoolClassManagement.DTOs;
+using MediatR;
+
+namespace Application.Features.SchoolClassManagement.Queries.GetAllClasses;
+
+public class GetAllClassesQueryHandler : IRequestHandler<GetAllClassesQuery, List<ClassResponseDto>>
+{
+    private readonly IClassRepository _classRepository;
+
+    public GetAllClassesQueryHandler(IClassRepository classRepository)
+    {
+        _classRepository = classRepository;
+    }
+
+    public Task<List<ClassResponseDto>> Handle(GetAllClassesQuery request, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
